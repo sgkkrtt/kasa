d19620adacd865dab7cc441d0a57c5187a22aa93
Son <gosauchit@gmail.com>
2025-10-24 15:33:53 +0200

    Création de tous les fichiers pages et components (vides)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
+import Header from "./components/header.jsx"
+import Footer from "./components/footer.jsx"
+import Home from "./pages/home.jsx"
+import About from "./pages/about.jsx"
+import NotFound from "./pages/404.jsx"
 import './App.css'
 
 const App = () => {
   return (
     <>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/logement/:id" element={<Logement />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+
+    <Footer />
     </>
   )
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return (
+        <h1>A</h1>
+    )
+}
+
+export default Footer
