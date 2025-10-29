1b6243ae4467b2a49b5c286d647c34c8934fcbe0
Son <gosauchit@gmail.com>
2025-10-29 18:49:25 +0100

    Fonctionnement du slider

--- src/components/slideshow.jsx
+++ src/components/slideshow.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "../styles/_slideshow.scss";
 import arrowLeft from "../assets/arrow-left.svg";
 import arrowRight from "../assets/arrow-right.svg";
@@ -5,23 +6,31 @@ import arrowRight from "../assets/arrow-right.svg";
 const Slideshow = ({ pictures }) => {
   if (!Array.isArray(pictures) || pictures.length === 0) return null;
 
+  const [index, setIndex] = useState(0);
+  const total = pictures.length;
+
+  const next = () => setIndex((index + 1) % total);
+  const prev = () => setIndex((index - 1 + total) % total);
+
   return (
     <div className="slideshow">
       <img
-        src={pictures[0]}
-        alt="Illustration du logement"
+        src={pictures[index]}
+        alt={`Illustration ${index + 1}`}
         className="slideshow__image"
       />
 
-      {pictures.length > 1 && (
+      {total > 1 && (
         <>
-          <button className="slideshow__arrow left">
+          <button className="slideshow__arrow left" onClick={prev}>
             <img src={arrowLeft} alt="flèche gauche" />
           </button>
-          <button className="slideshow__arrow right">
+          <button className="slideshow__arrow right" onClick={next}>
             <img src={arrowRight} alt="flèche droite" />
           </button>
-          <p className="slideshow__count">1/{pictures.length}</p>
+          <p className="slideshow__count">
+            {index + 1}/{total}
+          </p>
         </>
       )}
     </div>
